042876c57b058bedb9af09414f8439efa9ca0e6c
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-08-16 18:04:00 -0300

    fix: arrumado a animacao acontecendo em todos os cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,16 +2,26 @@ import React from "react";
 import heartEmpty from "../../assets/svg/heart-empty.svg";
 import heartFull from "../../assets/svg/heart-full.svg";
 import styles from "./Card.module.css";
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import { Link } from "react-router-dom";
 import { useFavoriteContext } from "../../context/FavoriteContext";
 
 const Card = ({ hero }) => {
-  const { isHeartFilled, toggleFavorite, controls } = useFavoriteContext();
+  const { isHeartFilled, toggleFavorite } = useFavoriteContext();
+  const controls = useAnimation();
+
+  const handleFavoriteClick = async () => {
+    await toggleFavorite(hero, controls);
+    controls.start({ scale: [1, 1.2, 1], opacity: [1, 0.8, 1] });
+  };
 
   return (
     <div className={styles.card} key={hero.id}>
-      <Link className={styles.img} to={`characters/${hero.id}`}>
+      <Link
+        className={styles.img}
+        to={`characters/${hero.id}`}
+        onClick={() => window.scrollTo(0, 0)}
+      >
         <span></span>
         <img
           src={`${hero.thumbnail.path}.${hero.thumbnail.extension}`}
@@ -21,10 +31,7 @@ const Card = ({ hero }) => {
 
       <div className={styles.info}>
         <h1>{hero.name}</h1>
-        <span
-          className={styles.favorite}
-          onClick={() => toggleFavorite(hero)}
-        >
+        <span className={styles.favorite} onClick={handleFavoriteClick}>
           <motion.img
             src={isHeartFilled(hero.id) ? heartFull : heartEmpty}
             alt="favorite"
