1d80733c8fe898ae1fdd1f0bd3d8c32c916e304f
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-08-16 17:46:01 -0300

    feat: adicionado classes a estrutura do skeleto do card

--- src/components/Skeleton/SkeletonCard.jsx
+++ src/components/Skeleton/SkeletonCard.jsx
@@ -1,18 +1,19 @@
 import React from "react";
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
+import styles from "./SkeletonCard.module.css";
 
 const SkeletonCard = () => {
   return (
     <SkeletonTheme color="#202020" highlightColor="#444" duration={1}>
-      <div data-testid="skeleton-loading">
-        <div>
-          <Skeleton height={248} />
+      <div className={styles.skeletonContainer} data-testid="skeleton-loading">
+        <div className={styles.skeletonImage}>
+          <Skeleton height="100%" />
         </div>
 
-        <div style={{display: "flex", justifyContent: "space-between", padding: "24px 0"}}>
-          <Skeleton width={150} height={18} />
-          <Skeleton width={18} height={18} />
+        <div className={styles.skeletonDetails}>
+          <Skeleton className={styles.skeletonText} />
+          <Skeleton className={styles.skeletonIcon} />
         </div>
       </div>
     </SkeletonTheme>
