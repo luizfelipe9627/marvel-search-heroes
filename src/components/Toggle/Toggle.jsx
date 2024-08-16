2fdeade6eacafa02e39484fa6e1903072ca96f34
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-08-16 16:42:25 -0300

    feat: criado componente responsavel por fazer a troca de filtro

---
+++ src/components/Toggle/Toggle.jsx
@@ -0,0 +1,69 @@
+import React from "react";
+import { motion } from "framer-motion";
+import styles from "./Toggle.module.css";
+import hero from "../../assets/svg/hero.svg";
+import heartFull from "../../assets/svg/heart-full.svg";
+import { useToggleContext } from "../../context/ToggleContext";
+import { useDataContext } from "../../context/DataContext";
+import { useSearchContext } from "../../context/SearchContext";
+
+const Toggle = () => {
+  const { toggleFavorite, setToggleFavorite } = useToggleContext();
+  const { dataAPI, isLoading } = useDataContext();
+  const { search } = useSearchContext();
+
+  const favoritesHeroes =
+    JSON.parse(localStorage.getItem("favoritesHeroes")) || [];
+
+  const filterHeroes = (heroes) =>
+    heroes.filter((hero) =>
+      hero.name.toLowerCase().includes(search.toLowerCase()),
+    );
+
+  const filteredCount = toggleFavorite
+    ? filterHeroes(favoritesHeroes).length
+    : dataAPI
+    ? filterHeroes(dataAPI.data.results).length
+    : 0;
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.panel}>
+        <div className={styles.count}>
+          {isLoading ? (
+            <p>Buscando heróis...</p>
+          ) : filteredCount === 1 ? (
+            <p>Encontrado 1 herói</p>
+          ) : (
+            <p>Encontrados {filteredCount} heróis</p>
+          )}
+        </div>
+      </div>
+
+      <div className={styles.toggle}>
+        <div className={styles.hero}>
+          <img src={hero} alt="Ícone de herói" />
+          <p>Ordenar por nome - A-Z</p>
+        </div>
+
+        <div
+          className={styles.button}
+          onClick={() => setToggleFavorite(!toggleFavorite)}
+        >
+          <motion.span
+            className={styles.circle}
+            animate={{ x: toggleFavorite ? 28 : 0 }}
+            transition={{ type: "spring", stiffness: 300 }}
+          />
+        </div>
+
+        <div className={styles.heart}>
+          <img src={heartFull} alt="Ícone de coração" />
+          <p>Somente favoritos</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Toggle;
