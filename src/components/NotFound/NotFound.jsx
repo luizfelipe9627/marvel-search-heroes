60beec2e3298feedb4adfa421f16b26f8d932840
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2024-08-16 16:39:25 -0300

    feat: componente de rota nao encontrada criado

---
+++ src/components/NotFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./NotFound.module.css";
+
+const NotFound = () => {
+  return (
+    <div className={styles.notFound}>
+      <h1>Página não encontrada.</h1>
+    </div>
+  );
+};
+
+export default NotFound;
